1fbe22469e3e3004a61a7c69708e1d84c917801b
daehan <dh16931@gmail.com>
2022-03-07 12:05:55 +0900

    feat: patent card component

---
+++ src/components/common/patentCard/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: ${({ theme }) => theme.colors.background};
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.text};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: auto;
+`;
+
+const PatentCard: FC = () => {
+    return <Container>Patent CARD</Container>;
+};
+
+export default PatentCard;
